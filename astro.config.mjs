72cf9197bde900d6d208fa2f192ed9f86ba4486b
yixxuz18 <yisuskroom@gmail.com>
2024-06-26 09:46:35 -0600

    cambios astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://telco-networks.mx",
+  site: "https://www.solaria.com.mx",
   integrations: [tailwind(),sitemap()],
 });
