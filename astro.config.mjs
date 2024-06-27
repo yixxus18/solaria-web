22d21944a4ba27cb0e7ffa124975887baf42d661
yixxuz18 <yisuskroom@gmail.com>
2024-06-27 08:58:19 -0600

    cambios en el bg de stats

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,6 @@ import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 
-// https://astro.build/config
 export default defineConfig({
   site: "https://www.solaria.com.mx",
   integrations: [tailwind(),sitemap()],
